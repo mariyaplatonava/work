e95c08c41a6097485c1c3e163558dfff418eae0d
PlatonovaMaria <mariya.platonava@innowise.com>
2025-11-20 13:37:34 +0100

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource source = new CancellationTokenSource(3000);
+            CancellationToken token = source.Token;
+
+            try
+            {
+                var result = Task.WhenAny(AsyncOperation(token));
+                if (result.Result.Status == TaskStatus.Canceled)
+                {
+                    Console.WriteLine("ПЛАГИН: Таймаут истек. Внешняя операция отменена.");
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        async static Task AsyncOperation(CancellationToken token)
+        {
+            await Task.Delay(10000, token);
+        }
+    }
+}
